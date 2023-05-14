0512e1b974738fd3ad59f8fea3d615ba42974d88
Noah <nkempers@r4nd0.com>
2023-05-15 00:08:35 +0200

    Delete forms.py

--- app/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField, SelectField, PasswordField
-from wtforms.validators import DataRequired, InputRequired, Length
-
-
-class BaseForm(FlaskForm):
-    number = IntegerField('Number', validators=[InputRequired()])
-    name = StringField('Name', validators=[Length(min=1, max=50),DataRequired()])
-    submit = SubmitField('Submit')
